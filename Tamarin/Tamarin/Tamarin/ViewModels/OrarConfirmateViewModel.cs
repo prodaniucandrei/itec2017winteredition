8239008b5d8062c4c3728a9afe8024debfa1eaf6
proda <prodaniucandrei@gmail.com>
2017-12-09 22:31:41 +0200

    List added to all pages (where needed)

--- Tamarin/Tamarin/Tamarin/ViewModels/OrarConfirmateViewModel.cs
+++ Tamarin/Tamarin/Tamarin/ViewModels/OrarConfirmateViewModel.cs
@@ -1,17 +1,62 @@
-﻿using Prism.Commands;
+﻿using Newtonsoft.Json;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Tamarin.Helpers;
+using Tamarin.Models;
+using Tamarin.Services;
+using Xamarin.Forms;
 
 namespace Tamarin.ViewModels
 {
     public class OrarConfirmateViewModel : BaseViewModel
     {
+
+        public ObservableRangeCollection<object> Materii { get; set; }
+        public Command LoadItemsCommand { get; }
+        public Command<StudentModel> ItemClickedCommand { get; }
         public OrarConfirmateViewModel(INavigationService navigationService) : base(navigationService)
         {
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+            ItemClickedCommand = new Command<StudentModel>(OnItemClicked);
+        }
 
+        private void OnItemClicked(object coleg)
+        {
+            //Colegi.Add(new { Nume = "Ronaldo" });
+        }
+        async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Materii.Clear();
+                var response = await StudentService.GetAll();
+                var content = await response.Content.ReadAsStringAsync();
+                var message = JsonConvert.DeserializeObject<List<object>>(content);
+                Materii.ReplaceRange(message);
+            }
+            catch (Exception ex)
+            {
+                MessagingCenter.Send(new ErrorMessageModel
+                {
+                    Title = "Error",
+                    Message = "Unable to load items.",
+                    Cancel = "OK"
+                }, "message");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
     }
 }
--- Tamarin/Tamarin/Tamarin/ViewModels/SubjectPrezentaViewModel.cs
+++ Tamarin/Tamarin/Tamarin/ViewModels/SubjectPrezentaViewModel.cs
@@ -1,17 +1,67 @@
-﻿using Prism.Commands;
+﻿using Newtonsoft.Json;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Tamarin.Helpers;
+using Tamarin.Models;
+using Tamarin.Services;
+using Xamarin.Forms;
 
 namespace Tamarin.ViewModels
 {
     public class SubjectPrezentaViewModel : BaseViewModel
     {
+        public DelegateCommand PrezentCommand { get; set; }
+        public ObservableRangeCollection<StudentModel> Studenti { get; set; }
+        public Command LoadItemsCommand { get; }
+        public Command<object> ItemClickedCommand { get; }
         public SubjectPrezentaViewModel(INavigationService navigationService) : base(navigationService)
         {
+            PrezentCommand = new DelegateCommand(OnPrezentSelected);
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+            ItemClickedCommand = new Command<object>(OnItemClicked);
+        }
 
+        private void OnItemClicked(object coleg)
+        {
+            //Colegi.Add(new { Nume = "Ronaldo" });
+        }
+        async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Studenti.Clear();
+                var response = await StudentService.GetAll();
+                var content = await response.Content.ReadAsStringAsync();
+                var message = JsonConvert.DeserializeObject<List<StudentModel>>(content);
+                Studenti.ReplaceRange(message);
+            }
+            catch (Exception ex)
+            {
+                MessagingCenter.Send(new ErrorMessageModel
+                {
+                    Title = "Error",
+                    Message = "Unable to load items.",
+                    Cancel = "OK"
+                }, "message");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+        public async void OnPrezentSelected()
+        {
+            //prezenta
         }
     }
 }
