8b966819f4184dd05ea0deb76b2c16016d11932b
proda <prodaniucandrei@gmail.com>
2017-12-09 11:03:45 +0200

    Schedule added

---
+++ Tamarin/Tamarin/Tamarin/ViewModels/ScheduleViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tamarin.ViewModels
+{
+    public class ScheduleViewModel : BindableBase,INavigationAware
+    {
+        INavigationService _navigationService;
+        public ScheduleViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+        }
+    }
+}
